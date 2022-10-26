7fc6ee1aa1261e6f015f0dfd6dbc5a322a7efa63
Md-abdullah-2001 <mdabdullah.dm2001@gmail.com>
2022-10-26 13:56:08 +0600

    updated single news api and added to client side

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 const courses = require("./data/majors.json");
+const courses_detail = require("./data/details.json");
 
 app.get("/", (req, res) => {
-  res.send("Server is Running");
+  res.send("courses");
+});
+app.get("/courses_detail/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses_detail.find((c) => c.id === id);
+  res.send(selectedCourse);
 });
 
+app.get("/courses_detail", (req, res) => {
+  res.send(courses_detail);
+});
 app.get("/courses-type", (req, res) => {
   res.send(courses);
 });
